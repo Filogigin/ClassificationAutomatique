3ea2b6513d58ac976a0c2c0a7732571cd3034afe
galianoe <enzo.galiano38@gmail.com>
2025-01-20 10:46:20 +0100

    5.2 a

--- src/PaireChaineEntier.java
+++ src/PaireChaineEntier.java
@@ -1,3 +1,25 @@
 public class PaireChaineEntier {
+    private String chaine;
+    private int entier;
 
+    public PaireChaineEntier(String chaine, int entier) {
+        chaine = this.chaine;
+        entier = this.entier;
+    }
+
+    public String getChaine() {
+        return chaine;
+    }
+
+    public int getEntier() {
+        return entier;
+    }
+
+    public void setChaine(String chaine) {
+        this.chaine = chaine;
+    }
+
+    public void setEntier(int entier) {
+        this.entier = entier;
+    }
 }
